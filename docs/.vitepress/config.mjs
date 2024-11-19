7eebc6726fa28254bfa71cd91817bbfb4e18a77f
ciretorres <erictorres.velasco@gmail.com>
2024-11-19 16:37:19 -0600

    perf: import pkg en lugar de require

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,7 +3,8 @@ import { createRequire } from 'module'
 
 const require = createRequire(import.meta.url)
 
-const pkg = require('../../package.json')
+// const pkg = require('../../package.json')
+import pkg from '../../package.json'
 
 function nav() {
   return [
